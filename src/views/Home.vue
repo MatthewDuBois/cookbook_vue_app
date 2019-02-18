d3a8e57c8e279bfd63289e1ce77ea801336f2629
Matthew DuBois <mattrdubois@gmail.com>
2019-02-18 16:36:36 -0600

    end of day

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Recipe</h1>
+    <div>
+      <div>
+        Title: <input v-model="newRecipeTitle">
+      </div>
+      <div>
+        Chef: <input v-model="newRecipeChef">
+      </div>
+      <div>
+        Prep Time: <input v-model="newRecipePrepTime">
+      </div>
+      <div>
+        Ingredients: <input v-model="newRecipeIngredients">
+      </div>
+      <div>
+        Directions: <input v-model="newRecipeDirections">
+      </div>
+      <div>
+        Image URL: <input v-model="newRecipeImageUrl">
+      </div>
+      <button v-on:click="createRecipe()">Create</button>
+    </div>
+
+    <h1>All Recipes</h1>
+   <div v-for="recipe in recipes">
+    <h1>{{ recipe.title }}</h1>
+    <img v-bind:src="recipe.image_url" v-bind:alt="recipe.title">
+    <p>Prep Time: {{ recipe.prep_time }}</p>
+    <p>Ingredients: {{ recipe.ingredients }}</p>
+    <p>Directions: {{ recipe.directions }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+  img{
+    width: 200px;
+  }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      recipes: [],
+      newRecipeTitle: "",
+      newRecipeChef: "",
+      newRecipePrepTime: "",
+      newRecipeIngredients: "",
+      newRecipeDirections: "",
+      newRecipeImageUrl: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/recipes")
+      .then(response => {
+        this.recipes = response.data;
+      });
+  },
+  methods: {
+    createRecipe: function() {
+      console.log("Create the Recipe...");
+      var params = {
+                    title: this.newRecipeTitle,
+                    chef: this.newRecipeChef,
+                    prep_time: this.newRecipePrepTime,
+                    ingredients: this.newRecipeIngredients,
+                    directions: this.newRecipeDirections,
+                    image_url: this.newRecipeImageUrl
+                    };
+      axios.post("/api/recipes", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.recipes.push(response.data);
+        });
+    }
   }
-}
+};
 </script>
